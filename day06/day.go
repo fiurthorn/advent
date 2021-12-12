a1da6dca3ff09929ee321cd4c84cfb8c23c01f54
Stefan Weinmann <s.weinmann@intex-online.de>
2021-12-12 08:53:51 +0100

    day six part 2

--- day06/day.go
+++ day06/day.go
@@ -24,25 +24,56 @@ func (d Day) Run() {
 func (d Day) process(data string) string {
 	nums := lib.Numbers(data)
 
-	return fmt.Sprintf("1:%v, 2:%v",
+	return fmt.Sprintf("1:%v, 1:%v, 2:%v",
+		d.process1(nums, 18),
 		d.process1(nums, 80),
-		"d.process1(nums, 256)",
+		d.process1(nums, 256),
 	)
 }
 
-// 1600306001288
 func (d Day) process1(numbers []int, days int) string {
-	for y := 0; y < days; y++ {
-		l := len(numbers)
-		for i := 0; i < l; i++ {
-			v := numbers[i]
-			if v == 0 {
-				numbers[i] = 6
-				numbers = append(numbers, 8)
-			} else {
-				numbers[i] = v - 1
-			}
-		}
+	var fishes = [9]uint64{}
+
+	for _, num := range numbers {
+		fishes[num]++
 	}
-	return fmt.Sprintf("%d", len(numbers))
+
+	for y := 0; y < days; y++ {
+		zero := fishes[0]
+
+		fishes[0] = fishes[1]
+		fishes[1] = fishes[2]
+		fishes[2] = fishes[3]
+		fishes[3] = fishes[4]
+		fishes[4] = fishes[5]
+		fishes[5] = fishes[6]
+		fishes[6] = fishes[7] + zero
+		fishes[7] = fishes[8]
+		fishes[8] = zero
+	}
+
+	result := fishes[0] +
+		fishes[1] +
+		fishes[2] +
+		fishes[3] +
+		fishes[4] +
+		fishes[5] +
+		fishes[6] +
+		fishes[7] +
+		fishes[8]
+
+	// for y := 0; y < days; y++ {
+	// 	fmt.Print("\r", y)
+	// 	l := len(numbers)
+	// 	for i := 0; i < l; i++ {
+	// 		v := numbers[i]
+	// 		if v == 0 {
+	// 			numbers[i] = 6
+	// 			numbers = append(numbers, 8)
+	// 		} else {
+	// 			numbers[i] = v - 1
+	// 		}
+	// 	}
+	// }
+	return fmt.Sprintf("%d", result)
 }
